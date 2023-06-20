222f335bee040f0e0f2b1acc444a1617ad4da062
Roseli Mariano <roseli.mariano26@gmail.com>
2023-06-20 15:38:23 -0300

    gulp-parte02

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,78 @@
+const { series } = require('gulp');
+
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
-const image = import('gulp-imagemin').then((module) => module.default);
-const stripJs = require('gulp-strip-comments')
+const stripJs = require('gulp-strip-comments');
 const stripCss = require('gulp-strip-css-comments');
+const htmlmin = require('gulp-htmlmin');
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 
-
-
-
-
-function tarefasCSS(cb) {
- 
-    return gulp.src([
-            './node_modules/bootstrap/dist/css/bootstrap.css',
-            './vendor/OwlCarousel/dist/assets/owl.carousel.css',
-           './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
-           './vendor/OwlCarousel/dist/assets/owl.theme.default.css',
-           './vendor/jquery-ui/jquery-ui.css',
-           './src/css/animate.css/animate.css',
-            'src/css/style.css',
-
-
-        ])
-        .pipe(stripCss())                   // remove comentários
-        .pipe(concat('styles.css'))         // mescla arquivos
-        .pipe(cssmin())                     // minifica css
-        .pipe(rename({ suffix: '.min'}))    // styles.min.css
-        .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
-
+function tarefasCSS() {
+  return gulp
+    .src([
+      './node_modules/bootstrap/dist/css/bootstrap.css',
+      './vendor/OwlCarousel/dist/assets/owl.carousel.css',
+      './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+      './vendor/OwlCarousel/dist/assets/owl.theme.default.css',
+      './vendor/jquery-ui/jquery-ui.css',
+      './src/css/animate.css/animate.css',
+      'src/css/style.css',
+    ])
+    .pipe(stripCss())
+    .pipe(concat('styles.css'))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/css'));
 }
 
+function tarefasJS() {
+  return gulp
+    .src([
+      './node_modules/jquery/dist/jquery.js',
+      './node_modules/bootstrap/dist/js/bootstrap.js',
+      './vendor/OwlCarousel/dist/owl.carousel.js',
+      './vendor/jquery-ui/jquery-ui.js',
+      './src/js/custom.js',
+    ])
+    .pipe(stripJs())
+    .pipe(concat('bundle.js'))
+    .pipe(babel({
+      comments: false,
+      presets: [
+        ['@babel/preset-env', {
+          targets: {
+            node: 'current',
+          },
+        }],
+      ],
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+      ],
+      generatorOpts: {
+        compact: false,
+        minified: false,
+        shouldPrintComment: "comment => /^[@#]__[A-Z]+__$/.test(comment)",
+        compactType: 'auto',
+      },
+    }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(babel()) // Aplicando o Babel novamente após a concatenação
+    .pipe(gulp.dest('./dist/js'));
+}
 
-function tarefasJS(){
-
-    return gulp.src([
-            './node_modules/jquery/dist/jquery.js',
-            './node_modules/bootstrap/dist/js/bootstrap.js',
-            './vendor/OwlCarousel/dist/owl.carousel.js',
-            './vendor/jQuery-Mask/jquery.mask.js',
-            './vendor/jquery-ui/jquery-ui.js',
-            './src/js/custom.js'
-        ])
-        .pipe(stripJs())                    // remove comentários
-        .pipe(concat('scripts.js'))         // mescla arquivos
-        .pipe(uglify())                     // minifica js
-        .pipe(rename({ suffix: '.min'}))    // scripts.min.js
-        .pipe(gulp.dest('./dist/js'))       // cria arquivo em novo diretório
-
-} 
-async function tarefasImagem() {
-    const imagemin = await image;
-    return gulp
-      .src('./src/imagem/*')
-      .pipe(imagemin({
+function tarefasImagem() {
+  return gulp
+    .src('./src/imagem/*')
+    .pipe(
+      imagemin({
         pngquant: true,
         optipng: false,
         zopflipng: true,
@@ -64,15 +81,43 @@ async function tarefasImagem() {
         gifsicle: true,
         svgo: true,
         concurrent: 10,
-        quiet: true
-      }))
-      .pipe(gulp.dest('./dist/images'));
-  }
-  
-  
+        quiet: true,
+      })
+    )
+    .pipe(gulp.dest('./dist/images'));
+}
+
+function tarefasHTML() {
+  return gulp
+    .src('./src/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist'));
+}
+
+async function processImages() {
+  await gulp
+    .src('./src/imagem/favicon-32x32.png')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+
+}
+
+gulp.task('server', function(){
+
+  browserSync.init({
+      server: {
+          baseDir: "./dist"
+      }
+  })
+  gulp.watch('./src/**/*').on('change', process) // repete o processo quando alterar algo em src
+  gulp.watch('./src/**/*').on('change', reload)
+
+})
+const process = series( tarefasHTML, tarefasJS, tarefasCSS)
 
 exports.styles = tarefasCSS;
 exports.scripts = tarefasJS;
 exports.imagem = tarefasImagem;
-
-  
+exports.html = tarefasHTML;
+exports.processImages = processImages;
+exports.default = process
--- src/js/custom.js
+++ src/js/custom.js
@@ -6,17 +6,10 @@ $(document).ready(function () {
     alert('Produto esgotado');
   });
 
-
-
-
-
   $('.item:first-child .btn-comprar').after('<span class="new-item">Novo</span>');
 });
 
-
-
 $(document).ready(function () {
-
   $('.piscar-image').owlCarousel({
     loop: true,
     items: 1,
@@ -25,36 +18,20 @@ $(document).ready(function () {
     animateOut: 'fadeOut',
     animateIn: 'fadeIn'
   });
-
-
-  $('.piscar-image').owlCarousel({
-    loop: true,
-    items: 1,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn'
-  });
-
-});;
+});
 
 $(document).ready(function () {
-
   let input = $('input[type="search"]');
-
 
   input.focus(function () {
     $(this).addClass('bg-primary');
   });
-
 
   input.blur(function () {
     $(this).removeClass('bg-primary');
     $(this).addClass('bg-light');
   });
 });
-
-
 
 $(document).ready(function () {
   $('.owl-carousel').owlCarousel({
@@ -81,7 +58,6 @@ $(document).ready(function () {
     animateIn: 'animate__zoomIn'
   });
 
-  // Máscaras para os campos
   $('#date').mask('00/00/0000');
   $('#email').mask('A', {
     translation: {
@@ -96,88 +72,77 @@ $(document).ready(function () {
   $('#hora').mask('00:00');
   $('#phone').mask('(00) 00000-0000');
 
-  // Validação do formulário
-  document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Impede o envio do formulário
+  $('#contact-form').on('submit', function (event) {
+    event.preventDefault();
 
-    // Remove as classes de erro dos campos
     $('.form-control').removeClass('border-danger');
-
-    // Restante do código de validação...
 
     let isValid = true;
 
-    // Função para adicionar a classe de erro
     function showError(input) {
       input.addClass('border-danger');
       isValid = false;
     }
 
-    // Validação do campo Nome
     let nomeInput = $('#nome');
-    if (nomeInput.val().trim() === "") {
+    if (nomeInput.val().trim() === '') {
       showError(nomeInput);
     }
 
-    // Validação do campo Email
     let emailInput = $('#email');
-    if (emailInput.val().trim() === "" || !isValidEmail(emailInput.val())) {
+    if (emailInput.val().trim() === '' || !isValidEmail(emailInput.val())) {
       showError(emailInput);
     }
 
-    // Validação do campo Data
     let dateInput = $('#date');
-    if (dateInput.val().trim() === "") {
+    if (dateInput.val().trim() === '') {
       showError(dateInput);
     }
 
-    // Validação do campo Hora
     let horaInput = $('#hora');
-    if (horaInput.val().trim() === "") {
+    if (horaInput.val().trim() === '') {
       showError(horaInput);
     }
 
-    // Validação do campo CEP
     cepInput = $('#cep');
-    if (cepInput.val().trim() === "") {
+    if (cepInput.val().trim() === '') {
       showError(cepInput);
     }
 
-    // Validação do campo Celular
     let phoneInput = $('#phone');
-    if (phoneInput.val().trim() === "") {
+    if (phoneInput.val().trim() === '') {
       showError(phoneInput);
     }
 
-    // Validação do campo CPF
     let cpfInput = $('#cpf');
-    if (cpfInput.val().trim() === "") {
+    if (cpfInput.val().trim() === '') {
       showError(cpfInput);
     }
 
-    // Se algum campo estiver inválido, exiba uma mensagem de erro
     if (!isValid) {
-      alert("Por favor, preencha todos os campos corretamente.");
+      alert('Por favor, preencha todos os campos corretamente.');
     } else {
-      // Simulação de envio do formulário (substitua com sua lógica real de envio)
       setTimeout(function () {
-        // Limpar os campos do formulário
         $('#contact-form')[0].reset();
-
-        // Exibir mensagem de envio bem-sucedido
-        alert("Formulário enviado com sucesso!");
+        alert('Formulário enviado com sucesso!');
       }, 1000);
     }
   });
 
-  // Função para validar o formato de e-mail
   function isValidEmail(email) {
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
 
-  // Datepicker
   $('#date').datepicker({
     showButtonPanel: true
   });
 });
+
+$(document).ready(function () {
+  $('#search-button').on('click', function () {
+    var mensagem = $('#search-input').val();
+    console.log('Mensagem enviada:', mensagem);
+  });
+});
+
